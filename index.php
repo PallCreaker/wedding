1068ecf3fb94aa2c7b43a7b3fe7cfba3bd7c8dce
iMac for local <pallcreaker@gmail.com>
2015-01-04 03:26:54 +0900

    next

--- index.php
+++ index.php
@@ -1,191 +1,191 @@
 <?php include 'header.php' ?>
-    <section id="top-view" class="ytplayer-dance">
-        <?php // https://github.com/pupunzi/jquery.mb.YTPlayer/wiki ?>
-        <a id="bgndVideo" class="player" 
-            data-property="{
-                videoURL:'//youtu.be/BT0hj6VTFzU',
-                containment:'.ytplayer-dance',
-                autoPlay:true,
-                showControls:true, //コントロールパネル表示
-                loop:true, // ループ（繰り返し）させる
-                mute:true, //music
-                startAt:0,
-                opacity:1,
-                optimizeDisplay:true, //will fit the video size into the window size optimizing the view.
-            }" title="dance wedding" style="position: relaticve;display:none;background-image:none">
-        </a>
-        <div class="bg-msg">
-            <span class="g-font italic">Dance Wedding</span>
-        </div>
-    </section>
-    <section id="about">
-        <div class="dance-msg">
-            <img src="img/ring.png" alt="結婚指輪">
-            <div class="detail top">
+<section id="top-view" class="ytplayer-dance">
+    <?php // https://github.com/pupunzi/jquery.mb.YTPlayer/wiki ?>
+    <a id="bgndVideo" class="player" 
+        data-property="{
+            videoURL:'//youtu.be/BT0hj6VTFzU',
+            containment:'.ytplayer-dance',
+            autoPlay:true,
+            showControls:true, //コントロールパネル表示
+            loop:true, // ループ（繰り返し）させる
+            mute:true, //music
+            startAt:0,
+            opacity:1,
+            optimizeDisplay:true, //will fit the video size into the window size optimizing the view.
+        }" title="dance wedding" style="position: relaticve;display:none;background-image:none">
+    </a>
+    <div class="bg-msg">
+        <span class="g-font italic">Dance Wedding</span>
+    </div>
+</section>
+<section id="about">
+    <div class="dance-msg">
+        <img src="img/ring.png" alt="結婚指輪">
+        <div class="detail top">
+            <div>
                 <div>
-                    <div>
-                        <h2>一緒に一度の<br>フラッシュモブプロポーズ</h2>
-                        <p>二人にとって<br>一生の一度のプロポーズを<br>素敵に演出してみませんか？</p>
-                    </div>
+                    <h2>一緒に一度の<br>フラッシュモブプロポーズ</h2>
+                    <p>二人にとって<br>一生の一度のプロポーズを<br>素敵に演出してみませんか？</p>
                 </div>
             </div>
         </div>
-        <div class="dance-msg">
-            <div class="detail bottom">
+    </div>
+    <div class="dance-msg">
+        <div class="detail bottom">
+            <div>
                 <div>
-                    <div>
-                        <h2>結婚式や誕生日会での<br>サプライズダンス</h2>
-                        <p>全く新しい<br>結婚式や誕生日会の演出を<br>一緒に作りませんか？</p>
-                    </div>
-                </div>
-            </div>
-            <img src="img/braide2.png"  alt="新郎新婦">
-        </div>
-    </section>
-    <section id="post">
-        <div class="head">
-            <img src="img/post_dance-wedding.png" alt="過去のDance Wedding">
-        </div>
-        <div class="movies">
-            <ul class="post-dance-movies cf">
-                <li>
-                    <iframe height="200" src="https://www.youtube.com/embed/ptuj-vsznmE?list=PL7lxl0JWdvX5dYT0jpuXZf1NvtAM96Tce" frameborder="0"></iframe>
-                    <p class="date">2014/5/24</p>
-                    <h3>
-                        `エレクトリカルパレード` で<br>フラッシュモブ サプライズ 
-                        <br>＠THE CLASSICA TOKYO
-                        <br> <span>with Crazy Wedding</span>
-                    </h3>
-                </li>
-                <li>
-                    <iframe height="200" src="https://www.youtube.com/embed/73iAkyFtfRc" frameborder="0"></iframe>
-                    <p class="date">2014/5/30</p>
-                    <h3>
-                        Pharrell Williams-Happyで結婚式
-                        <br>フラッシュモブダンスでサプライズ!!
-                        <br> <span>with Crazy Wedding</span>
-                    </h3>
-                </li>
-                <li>
-                    <iframe height="200" src="//www.youtube.com/embed/vFEuR3R87lM?list=PL7lxl0JWdvX5dYT0jpuXZf1NvtAM96Tce" frameborder="0" allowfullscreen></iframe>
-                    <p class="date"></p>
-                    <h3>
-                        【フラッシュモブ×フレッシュマン】
-                        <br>ダンスサプライズキックオフパーティー！！学園天国
-                        <br> <span>with Crazy Wedding</span>
-                    </h3>
-                </li>
-            </ul>
-        </div>
-    </section>
-    <section id="planner">
-        <div class="head">
-            <div><img src="img/p_d.png" alt="プランナー＆ダンサー紹介"></div>
-        </div>
-        <div class="planner menber-list">
-            <ul class="cf">
-                <li>
-                    <div>
-                        <img class="frame" src="img/intro_planner.png" alt="planner">
-                        <img class="people" src="img/p_d/riku.png" alt="リク">
-                        <p class="name">菊川　諒人</p>
-                        <img class="line" src="img/line_blue.png" alt="line">
-                        <p class="kana">Kikukawa Ryoto</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <img class="frame" src="img/intro_planner.png" alt="planner">
-                        <img class="people" src="img/p_d/riku.png" alt="リク">
-                        <p class="name">菊川　諒人</p>
-                        <img class="line" src="img/line_blue.png" alt="line">
-                        <p class="kana">Kikukawa Ryoto</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <img class="frame" src="img/intro_planner.png" alt="planner">
-                        <img class="people" src="img/p_d/riku.png" alt="リク">
-                        <p class="name">菊川　諒人</p>
-                        <img class="line" src="img/line_blue.png" alt="line">
-                        <p class="kana">Kikukawa Ryoto</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div class="dancer menber-list">
-            <ul class="cf">
-                <li>
-                    <div>
-                        <img class="frame" src="img/intro_dancer.png" alt="dancer">
-                        <img class="people" src="img/p_d/riku.png" alt="リク">
-                        <p class="name">菊川　諒人</p>
-                        <img class="line" src="img/line_purple.png" alt="line">
-                        <p class="kana">Kikukawa Ryoto</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <img class="frame" src="img/intro_dancer.png" alt="dancer">
-                        <img class="people" src="img/p_d/riku.png" alt="リク">
-                        <p class="name">菊川　諒人</p>
-                        <img class="line" src="img/line_purple.png" alt="line">
-                        <p class="kana">Kikukawa Ryoto</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <img class="frame" src="img/intro_dancer.png" alt="dancer">
-                        <img class="people" src="img/p_d/riku.png" alt="リク">
-                        <p class="name">菊川　諒人</p>
-                        <img class="line" src="img/line_purple.png" alt="line">
-                        <p class="kana">Kikukawa Ryoto</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div class="pink-bar"></div>
-    </section>
-    <section id="contact">
-        <div class="inner cf">
-            <div class="act">
-                <img class="top" src="img/groom_picture_top.png" height="320" width="285" alt="">
-                <img class="bottom" src="img/bride_picture_bottom.png" height="324" width="284" alt="">
-            </div>
-            <div class="detail">
-                <img src="img/bg_flower_top.png" alt="花">
-                <p class="outline">コンセプト設計、曲編集、ダンサーキャスティング、ダンスの振り付けなどのお客様のご要望に合わせて行うことができます。</p>
-                <p class="more">価格・実施までの流れは <a href="./price_and_flow"><img src="img/btn_more.png"  alt="こちら"></a></p>
-                <div class="tel"><img src="img/tel.png" alt="お電話"></div>
-                <div class="inquiry">
-                    <a href="./price_and_flow"><p><img src="img/pc.png" alt="">Webからのお問い合せは こちらから</p></a>
-                </div>
-                <img class="hana" src="img/bg_flower_bottom.png" alt="花">
-            </div>
-        </div>
-    </section>
-    <section id="future">
-        <div class="inner">
-            <div class="head"><img src="img/future.png" alt="弊社の３つの特徴"></div>
-            <div class="outl">
-                <dl>
-                    <dt><img src="img/number1.png" alt="1"></dt>
-                    <dd><span class="m-yellow">ダンサーのプロダクション</span>だから、プロダンサーが振り付け・構成制作・事前レッスン・当日の演出を行います。</dd>
-                </dl>
-                <dl>
-                    <dt><img src="img/number2.png" alt="2"></dt>
-                    <dd><span class="m-yellow">完全オーダーメイド</span>で振り付け制作、演出を一緒に考えさせていただきます。</dd>
-                </dl>
-                <dl>
-                    <dt><img src="img/number3.png" alt="3"></dt>
-                    <dd>プライダルの企業様と一緒に提案させて頂いた<span class="m-yellow">実績</span>があります。</dd>
-                </dl>
-                <div class="support">
-                    <p><<span class="m-red">協力企業様</span>></p>
-                    <p class="company">株式会社 crazy wedding <br>
-                    株式会社 INNOVATION DESIGN</p>
+                    <h2>結婚式や誕生日会での<br>サプライズダンス</h2>
+                    <p>全く新しい<br>結婚式や誕生日会の演出を<br>一緒に作りませんか？</p>
                 </div>
             </div>
         </div>
-    </section>
+        <img src="img/braide2.png"  alt="新郎新婦">
+    </div>
+</section>
+<section id="post">
+    <div class="head">
+        <img src="img/post_dance-wedding.png" alt="過去のDance Wedding">
+    </div>
+    <div class="movies">
+        <ul class="post-dance-movies cf">
+            <li>
+                <iframe height="200" src="https://www.youtube.com/embed/ptuj-vsznmE?list=PL7lxl0JWdvX5dYT0jpuXZf1NvtAM96Tce" frameborder="0"></iframe>
+                <p class="date">2014/5/24</p>
+                <h3>
+                    `エレクトリカルパレード` で<br>フラッシュモブ サプライズ 
+                    <br>＠THE CLASSICA TOKYO
+                    <br> <span>with Crazy Wedding</span>
+                </h3>
+            </li>
+            <li>
+                <iframe height="200" src="https://www.youtube.com/embed/73iAkyFtfRc" frameborder="0"></iframe>
+                <p class="date">2014/5/30</p>
+                <h3>
+                    Pharrell Williams-Happyで結婚式
+                    <br>フラッシュモブダンスでサプライズ!!
+                    <br> <span>with Crazy Wedding</span>
+                </h3>
+            </li>
+            <li>
+                <iframe height="200" src="//www.youtube.com/embed/vFEuR3R87lM?list=PL7lxl0JWdvX5dYT0jpuXZf1NvtAM96Tce" frameborder="0" allowfullscreen></iframe>
+                <p class="date"></p>
+                <h3>
+                    【フラッシュモブ×フレッシュマン】
+                    <br>ダンスサプライズキックオフパーティー！！学園天国
+                    <br> <span>with Crazy Wedding</span>
+                </h3>
+            </li>
+        </ul>
+    </div>
+</section>
+<section id="planner">
+    <div class="head">
+        <div><img src="img/p_d.png" alt="プランナー＆ダンサー紹介"></div>
+    </div>
+    <div class="planner menber-list">
+        <ul class="cf">
+            <li>
+                <div>
+                    <img class="frame" src="img/intro_planner.png" alt="planner">
+                    <img class="people" src="img/p_d/riku.png" alt="リク">
+                    <p class="name">菊川　諒人</p>
+                    <img class="line" src="img/line_blue.png" alt="line">
+                    <p class="kana">Kikukawa Ryoto</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="frame" src="img/intro_planner.png" alt="planner">
+                    <img class="people" src="img/p_d/riku.png" alt="リク">
+                    <p class="name">菊川　諒人</p>
+                    <img class="line" src="img/line_blue.png" alt="line">
+                    <p class="kana">Kikukawa Ryoto</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="frame" src="img/intro_planner.png" alt="planner">
+                    <img class="people" src="img/p_d/riku.png" alt="リク">
+                    <p class="name">菊川　諒人</p>
+                    <img class="line" src="img/line_blue.png" alt="line">
+                    <p class="kana">Kikukawa Ryoto</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="dancer member-list">
+        <ul class="cf">
+            <li>
+                <div>
+                    <img class="frame" src="img/intro_dancer.png" alt="dancer">
+                    <img class="people" src="img/p_d/riku.png" alt="リク">
+                    <p class="name">菊川　諒人</p>
+                    <img class="line" src="img/line_purple.png" alt="line">
+                    <p class="kana">Kikukawa Ryoto</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="frame" src="img/intro_dancer.png" alt="dancer">
+                    <img class="people" src="img/p_d/riku.png" alt="リク">
+                    <p class="name">菊川　諒人</p>
+                    <img class="line" src="img/line_purple.png" alt="line">
+                    <p class="kana">Kikukawa Ryoto</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="frame" src="img/intro_dancer.png" alt="dancer">
+                    <img class="people" src="img/p_d/riku.png" alt="リク">
+                    <p class="name">菊川　諒人</p>
+                    <img class="line" src="img/line_purple.png" alt="line">
+                    <p class="kana">Kikukawa Ryoto</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="pink-bar"></div>
+</section>
+<section id="contact">
+    <div class="inner cf">
+        <div class="act">
+            <img class="top" src="img/groom_picture_top.png" height="320" width="285" alt="">
+            <img class="bottom" src="img/bride_picture_bottom.png" height="324" width="284" alt="">
+        </div>
+        <div class="detail">
+            <img src="img/bg_flower_top.png" alt="花">
+            <p class="outline">コンセプト設計、曲編集、ダンサーキャスティング、ダンスの振り付けなどのお客様のご要望に合わせて行うことができます。</p>
+            <p class="more">価格・実施までの流れは <a href="./price_and_flow"><img src="img/btn_more.png"  alt="こちら"></a></p>
+            <div class="tel"><img src="img/tel.png" alt="お電話"></div>
+            <div class="inquiry">
+                <a href="./price_and_flow"><p><img src="img/pc.png" alt="">Webからのお問い合せは こちらから</p></a>
+            </div>
+            <img class="hana" src="img/bg_flower_bottom.png" alt="花">
+        </div>
+    </div>
+</section>
+<section id="future">
+    <div class="inner">
+        <div class="head"><img src="img/future.png" alt="弊社の３つの特徴"></div>
+        <div class="outl">
+            <dl>
+                <dt><img src="img/number1.png" alt="1"></dt>
+                <dd><span class="m-yellow">ダンサーのプロダクション</span>だから、プロダンサーが振り付け・構成制作・事前レッスン・当日の演出を行います。</dd>
+            </dl>
+            <dl>
+                <dt><img src="img/number2.png" alt="2"></dt>
+                <dd><span class="m-yellow">完全オーダーメイド</span>で振り付け制作、演出を一緒に考えさせていただきます。</dd>
+            </dl>
+            <dl>
+                <dt><img src="img/number3.png" alt="3"></dt>
+                <dd>プライダルの企業様と一緒に提案させて頂いた<span class="m-yellow">実績</span>があります。</dd>
+            </dl>
+            <div class="support">
+                <p><<span class="m-red">協力企業様</span>></p>
+                <p class="company">株式会社 crazy wedding <br>
+                株式会社 INNOVATION DESIGN</p>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include 'footer.php' ?>
--- price_and_flow.php
+++ price_and_flow.php
@@ -1,4 +1,80 @@
 <?php include 'header.php' ?>
-    <section>
-    </section>
+<section id="suprise-dance">
+    <div class="bg-msg">
+        <h2>
+            サプライズダンスで<br>
+            大切な人に<br>
+            想いを伝えませんか？
+        </h2>
+        <p>
+            ダンスを中心とするアーティストのプロダクションのため、<br>
+            プロが直接教えます。<br>
+            ご要望に合わせてオーダーメイドでゼロから作ります。
+        </p>
+    </div>
+</section>
+<section id="sean">
+    <div class="use"><img src="img/sean.png" alt="ご利用シーン"></div>
+</section>
+<section id="flow">
+    <div class="head"><img src="img/flow.png" alt="実施までの流れ"></div>
+    <div class="flow">
+        <ul class="cf">
+            <li>
+                <div>
+                    <img class="f-icon" src="img/flower_icon.png" alt="花">
+                    <img class="title" src="img/flow/1.png" alt="お問い合わせ">
+                    <p class="outline">担当プランナーまで何でもお声掛けください。</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="f-icon" src="img/flower_icon.png" alt="花">
+                    <img class="title" src="img/flow/2.png" alt="相談会">
+                    <p class="outline">ご要望をお伺いしながら、事例紹介や見積もりをさせて頂きます。</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="f-icon" src="img/flower_icon.png" alt="花">
+                    <img class="title" src="img/flow/3.png" alt="お申込み">
+                    <p class="outline">1ヶ月前までにお申込み下さい。</p>
+                </div>
+            </li>
+        </ul>
+        <ul class="cf">
+            <li>
+                <div>
+                    <img class="f-icon" src="img/flower_icon.png" alt="花">
+                    <img class="title" src="img/flow/4.png" alt="演出内容・打ち合わせ">
+                    <p class="outline">3週間〜1ヶ月前。<br>ご要望を伺い、演出・振り付けを一から考えさせて頂きます。</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="f-icon" src="img/flower_icon.png" alt="花">
+                    <img class="title" src="img/flow/5.png" alt="事前レッスン">
+                    <p class="outline">1〜3週間前。<br>プロダンサーの指導のもと、レッスンを実施します。</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <img class="f-icon" src="img/flower_icon.png" alt="花">
+                    <img class="title" src="img/flow/6.png" alt="当日">
+                    <p class="outline">思いっきり踊って、大切な人に想いを伝えましょう！</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+<section id="price">
+        <div class="table">
+            <table>
+
+            </table>
+        </div>
+</section>
+<section id="form">
+    
+</section>
 <?php include 'footer.php' ?>
