bdaef87697e3785f97e41d1ba338111995cc196a
kazumasa Fujita <pallcreaker@gmail.com>
2015-01-07 14:17:40 +0900

    微修正

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
             <div class="phone"><img src="img/footer_tel.png" alt="お問い合わせ"></div>
             <nav>
                 <ul class="cf">
-                    <li><a href="">会社概要</a></li> 
-                    <li><a href="#about">Dance Weddingについて</a></li>
-                    <li><a href="#planner">所属ダンサー</a></li>
-                    <li><a href="#contact">お問い合わせ</a></li>
+                    <li><a href="http://ostance.com" target="_blank">会社概要</a></li> 
+                    <li><a href="./#about">Dance Weddingについて</a></li>
+                    <li><a href="./price_and_flow">価格と実際の流れ</a></li>
+                    <li><a href="./#contact">お問い合わせ</a></li>
                 </ul>
             </nav>
-            <div class="copyright cf">Copyright 2015 <a href="http://ostance.com">Ostance</a> All Rights Reserved</div>
+            <div class="copyright cf">Copyright 2015 <a href="http://ostance.com"  target="_blank">Ostance</a> All Rights Reserved</div>
         </div>
     </footer>
 </body>
